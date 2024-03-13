8bbf693988cb4828bec1bfcdfb4ba857d1e7c555
Erick Monteiro <85596186+BiraBalaZ@users.noreply.github.com>
2024-03-13 20:39:23 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,26 @@
-function calcular()
-    {
-        var limite = 50; //Definindo o limite de velocidade.
-        var txtv = document.getElementById('txtvel');
-        var res = document.getElementById('res');
-        var vel = Number(txtv.value) //Pegando em numero o valor digitado no input com Id 'txtv'.
+function calcular() {
+    var limite = 70; //Definindo o limite de velocidade.
+    var txtv = document.getElementById('txtvel');
+    var res = document.getElementById('res');
+    var vel = Number(txtv.value) //Pegando em numero o valor digitado no input com Id 'txtv'.
 
-        //Exibindo a velocidade em Km/h que a pessoa escolheu.
+    //Exibindo a velocidade em Km/h que o usuário "escolheu".
+    if (vel > 0) {
         res.innerHTML = `<p>Sua velocidade atual é de ${vel} Km/h.</p>`;
-
-        //Se a velocidade for maior que 50, ele exibe a mensagem de multa.
-        if (vel >= limite)
-        {
-            res.innerHTML += '<p>Você foi <strong>multado</strong>!</p>';
-        }
-        else //Senão, ele exibe a mensagem de consciencia no volante.
-        {
-            res.innerHTML += '<p>Continue consciente no volante</p>';
-        }
     }
+
+    //Se a velocidade for maior que o limite, ele exibe a mensagem de multa.
+    //Senão, ele exibe a mensagem de consciencia no volante.
+    if (vel > limite) {
+        res.innerHTML += `<p> O limite é ${limite} Km/h! Você foi <strong>multado</strong>!</p>`;
+    }
+    else if (vel == 0) {
+        res.innerHTML = '<p>...</p>';
+    }
+    else if (vel < 0) {
+        res.innerHTML = '<p>Você foi <strong>multado</strong> pela gracinha.</p>';
+    }
+    else {
+        res.innerHTML += '<p>Continue consciente no volante :)</p>';
+    }
+}
